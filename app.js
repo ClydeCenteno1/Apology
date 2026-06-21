81d3980d47d38b191691db88cf908eff83890688
First Commit <ClydeCenteno1@example.com>
2026-06-21 13:02:32 +0800

    fix music player

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const bearGif = document.querySelector("img")
 const music = document.getElementById("bg-music");
-let musicStarted = false;
 
 bearGif.addEventListener("mouseenter", () => {
     bearGif.src="https://media.tenor.com/x3tbINZF4K4AAAAC/milk-and-mocha-bear-together.gif"
-    
-    // Play music on hover
-    if (!musicStarted) {
-        music.play().catch(error => {
-            console.log("Playback prevented by browser:", error);
-        });
-        musicStarted = true;
-    }
+    music.muted = false;
 })
 
 bearGif.addEventListener("mouseleave", () => {
